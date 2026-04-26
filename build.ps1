c368bb0853516a80465e848b086c383c160aba16
Jiwen63 <wenr2025@qq.com>
2026-04-26 22:32:40 +0800

    Add build.ps1

---
+++ build.ps1
@@ -0,0 +1,3 @@
+$env:PATH = "C:\Users\DELL\.workbuddy\binaries\node\versions\20.18.0.installing.25660.__extract_temp__\node-v20.18.0-win-x64;$env:PATH"
+cd "C:\Users\DELL\WorkBuddy\20260426182247\scuti-app"
+node node_modules/vite/bin/vite.js build 2>&1
